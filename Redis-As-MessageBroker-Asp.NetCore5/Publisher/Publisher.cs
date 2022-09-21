923ad05636c0c21badf475b04ded952f07eeda56
Talha-Jabbar-SDE <talha.jabbar@logicielservice.com>
2022-09-21 20:05:35 +0500

    init

---
+++ Redis-As-MessageBroker-Asp.NetCore5/Publisher/Publisher.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Publisher
+{
+    internal class Publisher
+    {
+        private const string RedisConnectionString = "localhost:6379";
+        private static ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(RedisConnectionString);
+        private const string Channel = "test-channel"; // Pub/Sub Channel
+        static void Main(string[] args)
+        {
+            var pub = connection.GetSubscriber();
+
+            pub.PublishAsync(Channel, "This is a test message!!", CommandFlags.FireAndForget);
+            Console.Write("Message Successfully sent to test-channel");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Redis-As-MessageBroker-Asp.NetCore5/Subscriber/Subscriber.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Subscriber
+{
+    internal class Subscriber
+    {
+        private const string RedisConnectionString = "localhost:6379";
+        private static ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(RedisConnectionString);
+        private const string Channel = "test-channel"; // Pub/Sub Channel
+        private const string KeyspaceChannel = "__keyspace*__:mykey"; // Keyspace notifications
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listening test-channel");
+            var sub = connection.GetSubscriber();
+
+            sub.Subscribe(Channel, (channel, message) => Console.Write("Message received from test-channel : " + message + "\n"));
+
+            sub.Subscribe(KeyspaceChannel, (channel, value) => Console.Write("Following command is executed on mykey : " + value + "\n"));
+
+            Console.ReadLine();
+        }
+    }
+}
